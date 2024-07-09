5e63c3cb19056397cc092b2e70bf1f6282755d6c
Hassan Sobane <hsobane@e3r7p4.1337.ma>
2024-07-09 12:15:39 +0100

    new haeders added

---
+++ include/Response.hpp
@@ -0,0 +1,10 @@
+#ifndef __RESPONSE_HPP__
+# define __RESPONSE_HPP__
+
+class	Response {
+public:
+	Response();
+	~Response();
+};
+
+#endif
--- include/Server.hpp
+++ include/Server.hpp
@@ -3,6 +3,8 @@
 
 # include "Location.hpp"
 # include "CGI.hpp"
+# include "Request.hpp"
+# include "Response.hpp"
 # include <string>
 # include <vector>
 # include <map>
@@ -19,14 +21,18 @@ public:
 
 private:
 	/*CONFIG START*/
-	std::vector<Location>		_locations;
-	std::string					_server_name;
-	int							_port;
-	std::string					_host;
-	std::map<int, std::string>	_error_pages;
-	size_t						_client_max_body_size;
+	std::vector<Location>								_locations;
+	std::string											_server_name;
+	int													_port;
+	std::string											_host;
+	std::map<int, std::string>							_error_pages;
+	size_t												_client_max_body_size;
 	/*CONFIG END*/
 
+	int													_socket;
+	std::map<int, std::pair<Request*, Response*> >		_requests;
+	std::map<int, CGI>									_cgi;
+	
 };
 
 #endif

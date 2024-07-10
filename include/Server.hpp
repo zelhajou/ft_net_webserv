c0b03271bc6e07868e60e1e92e82b40fd40ef1f9
Hassan Sobane <hsobane@e3r4p1.1337.ma>
2024-07-10 02:14:00 +0100

    ok

--- include/Server.hpp
+++ include/Server.hpp
@@ -14,6 +14,8 @@ public:
 	Server();
 	~Server();
 
+	std::string		getHost() const;
+	std::string		getPort() const;
 	/*Parse Methods Start */
 
 	/*Parse Methods End */
@@ -23,14 +25,14 @@ private:
 	/*CONFIG START*/
 	std::vector<Location>								_locations;
 	std::string											_server_name;
-	int													_port;
+	std::string											_port;
 	std::string											_host;
 	std::map<int, std::string>							_error_pages;
 	size_t												_client_max_body_size;
 	/*CONFIG END*/
 
 	int													_socket;
-	std::map<int, std::pair<Request*, Response*> >		_requests;
+	std::map<int, std::pair<Request, Response> >		_requests;
 	std::map<int, CGI>									_cgi;
 	
 };
--- include/Sockets.hpp
+++ include/Sockets.hpp
@@ -2,9 +2,17 @@
 # define __SOCKETS_HPP__
 
 # include <map>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include "Server.hpp"
 # include "KQueue.hpp"
 # include "Parser.hpp"
+# include "util.h"
+
+typedef std::map<int, Server *>::iterator mit;
 
 class Sockets {
 public:
@@ -12,8 +20,15 @@ public:
 	~Sockets();
 
 	void						start();
+	void						accept();
+	void						recvFrom();
+	void						sendTo();
+	void						closeCon();
+	void						cleanUp();
+	void						kqueueLoop();
 
 private:
+	MIME						_mime;
 	Parser&						_parser;
 	KQueue						_kqueue;
 	std::map<int, Server *>		_fd_to_server;

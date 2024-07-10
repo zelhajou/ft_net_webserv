41d46552a33c73b54ec8dbf67f35e591891dd2f9
Hassan Sobane <hsobane@e3r4p1.1337.ma>
2024-07-10 02:14:00 +0100

    ok

--- include/Request.hpp
+++ include/Request.hpp
@@ -3,6 +3,7 @@
 
 # include <string>
 # include <map>
+# include <fstream>
 # include "util.h"
 
 class Request {
@@ -10,11 +11,18 @@ public:
 	Request();
 	~Request();
 
+	void	POST();
+
 private:
+	int					_fd;
+	std::ifstream		_file;
+	size_t				_recv;
 	t_first_line		_first_line;
 	t_headers			_headers;
 	std::string			_body;
 	e_parser_state		_state;
+	e_status			_status;
+	size_t				_timeout;
 	
 
 };
--- include/Response.hpp
+++ include/Response.hpp
@@ -1,10 +1,32 @@
 #ifndef __RESPONSE_HPP__
 # define __RESPONSE_HPP__
 
+# include <string>
+# include <fstream>
+# include "util.h"
+# include "Request.hpp"
+
+# define BUFFER_SIZE 4096
+
 class	Response {
 public:
-	Response();
+	Response(Request &req);
 	~Response();
+
+	void	setRequest(Request &req);
+	void	sendResponse();
+	void	GET();
+	void	DELETE();
+
+private:
+	int					_fd;
+	std::ifstream		_file;
+	size_t				_sent;
+	Request				&_req;
+	t_first_line		_first_line;
+	t_headers			_headers;
+	std::string			_body;
+	e_status			_status;
 };
 
 #endif
